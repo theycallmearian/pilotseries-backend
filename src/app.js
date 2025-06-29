10e9e6ccb2488fb5f17388b2a8e3078878bbb861
Àrian <153982767+theycallmearian@users.noreply.github.com>
2025-06-29 23:03:48 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,12 +5,6 @@ require('dotenv').config()
 
 const app = express()
 app.use(express.json())
-app.use(
-  cors({
-    origin: ['http://localhost:5173', process.env.FRONTEND_URL].filter(Boolean),
-    credentials: true
-  })
-)
 
 mongoose
   .connect(process.env.MONGODB_URI, {
